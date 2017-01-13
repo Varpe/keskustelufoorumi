d916a6fddeb8c11d0e65fc3742d5ac7079a8a39c
Erja Nikunen <erja.nikunen@gmail.com>
2017-01-13 14:23:44 +0000

    pävitetty uudemmat versiot package.jsoniin

--- bin/www.js
+++ bin/www.js
@@ -1,10 +1,5 @@
-#!/usr/bin/env node
 
-/**
- * Module dependencies.
- */
-
-var app = require('../app');
+var app = require('../app.js');
 var debug = require('debug')('Foorumi:server');
 var http = require('http');
 
