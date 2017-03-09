016b7546201dfa82d047b0343c1d52a79893c720
Joonas Varpelaide <joonas.varpelaide@metropolia.fi>
2017-03-09 14:41:55 +0000

    Sovellus Herokuun

--- db/connection.js
+++ db/connection.js
@@ -1,17 +1,17 @@
-/*
 var Sequelize = require('sequelize');
 var sequelize = new
-Sequelize('postgres://cklfpkgwtnknxa:b97b6cf8580217dc8b7d7eecc7c7c80923d267a160d028229c9c7b019a29d551@ec2-54-163-224-108.compute-1.amazonaws.com:5432/deiho4so3aagqj', {
+Sequelize('postgres://vzeimjppyoksfh:eda9114fddcf4bf91c190db5a04f6ef6d217d8306774cd89f1fb68b0dca2e2d6@ec2-54-243-252-91.compute-1.amazonaws.com:5432/d80nll27qcq5jo', {
  dialect: 'postgres',
  protocol: 'postgres'
 });
-*/
+
+/*
 var Sequelize = require('sequelize');
 var sequelize = new Sequelize('foorumi', '', '', {
   dialect: 'sqlite',
   storage: '../db/database.sqlite'
 });
-
+*/
 
 module.exports = {
   DataTypes: Sequelize,
